95adeb09815978152a2da9db18005478084ffa00
Arun Biradar <arunvbiradar@gmail.com>
2019-10-09 19:35:20 +0530

    JWT token returned on registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("./../../models/Users");
@@ -54,7 +56,20 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("user registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Server error");
