719ee35ce412524b62b9eb75dc63a64641b2b6e4
Arun Biradar <arunvbiradar@gmail.com>
2019-10-10 01:35:14 +0530

    Get logged in users profile

--- models/Profile.js
+++ models/Profile.js
@@ -50,17 +50,19 @@ const ProfileSchema = new mongoose.Schema({
       description: { type: String }
     }
   ],
-  social: {
-    youtube: { type: String },
-    twitter: { type: String },
-    facebook: { type: String },
-    linkedin: { type: String },
-    instagram: { type: string }
-  },
+  social: [
+    {
+      youtube: { type: String },
+      twitter: { type: String },
+      facebook: { type: String },
+      linkedin: { type: String },
+      instagram: { type: String }
+    }
+  ],
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = Profile = mongoose.model("profile", "ProfileSchema");
+module.exports = profile = mongoose.model("profile", ProfileSchema);
