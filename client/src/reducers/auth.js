4a8ba1b29c7ebeaacc74f510d86ab3f11297ee02
Arun Biradar <arunvbiradar@gmail.com>
2019-10-14 17:37:32 +0530

    Logout functionality

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   AUTH_ERROR,
-  USER_LOADED
+  USER_LOADED,
+  LOGOUT
 } from "./../actions/types";
 
 const initialState = {
@@ -31,6 +32,7 @@ export default function(state = initialState, action) {
     case REGISTER_FAIL:
     case LOGIN_FAIL:
     case AUTH_ERROR:
+    case LOGOUT:
       localStorage.removeItem("token");
       return { ...state, token: null, isAuthenticated: false, loading: false };
     default:
