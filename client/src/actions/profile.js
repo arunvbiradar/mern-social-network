1d17b8b14744b5de20ed71b3f2ef46ef0ddf4295
Arun Biradar <arunvbiradar@gmail.com>
2019-10-15 00:24:45 +0530

    Create Profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { setAlert } from "./alert";
 
 // get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -10,6 +11,45 @@ export const getCurrentProfile = () => async dispatch => {
       payload: res.data
     });
   } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: error.response.statusText, status: error.response.status }
