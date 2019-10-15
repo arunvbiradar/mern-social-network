b685ff3e45e39b5d400de9014456e5a22c03da9c
Arun Biradar <arunvbiradar@gmail.com>
2019-10-15 14:46:17 +0530

    Get all profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import {
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
@@ -14,6 +16,60 @@ export const getCurrentProfile = () => async dispatch => {
     const res = await axios.get("api/profile/me");
     dispatch({
       type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// get all profile
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get("api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// get all profile by id
+export const getProfileById = userId => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// get githun repos
+export const getGithubRepos = username => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
       payload: res.data
     });
   } catch (error) {
---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const ProfileItem = ({
+  profile: {
+    user: { _id, name, avatar },
+    status,
+    company,
+    location,
+    skills
+  }
+}) => {
+  return (
+    <div className='profile bg-light'>
+      <img className='round-img' src={avatar} alt={name} />
+      <div>
+        <h2>{name}</h2>
+        <p>{company}</p>
+        <p>{location}</p>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+          View Profile
+        </Link>
+      </div>
+
+      <ul>
+        {skills.slice(0, 4).map((skill, index) => (
+          <li className='text-primary' key={index}>
+            <i className='fa fa-check'></i> {skill}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+ProfileItem.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+export default ProfileItem;
